b845ac824efe80871878601dcacfc36749f3c5cc
Goretti <Goretti@Goretti>
2015-10-15 09:47:15 +0100

    Practica 3 - Muestra un Histograma con mas datos (Enero y Frebero)

--- src/is2_practica3/HistogramDisplay.java
+++ src/is2_practica3/HistogramDisplay.java
@@ -32,17 +32,20 @@ public class HistogramDisplay extends ApplicationFrame {
                 "Nº email",
                 dataset,
                 PlotOrientation.VERTICAL,
-                false,
-                false, 
-                false);
+                true, //Sirve para que muestre leyenda en caso de tener mas datos
+                true, // Este es pa que al pasar el raton por encim te muestre los datos
+                false); //Este es pa entorno web
         return chart;
     }
     
     private DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(10, "", "gmail.com");
-        dataset.addValue(7, "", "ulpgc.com");
-        dataset.addValue(10, "", "ull.com");
+        dataset.addValue(10, "ENE", "gmail.com");
+        dataset.addValue(7, "ENE", "ulpgc.com");
+        dataset.addValue(10, "ENE", "ull.com");
+        dataset.addValue(17, "FEB", "gmail.com");
+        dataset.addValue(7, "FEB", "ulpgc.com");
+        dataset.addValue(10, "FEB", "ull.com");
         return dataset;
     }
     
