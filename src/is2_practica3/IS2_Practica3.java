b1f193fe2f4ffeefdad1935706af12e49b0afa09
Goretti <Goretti@Goretti>
2015-10-15 08:42:38 +0100

    Practica 3 sin codigo - 15/10/2015

---
+++ src/is2_practica3/IS2_Practica3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is2_practica3;
+
+/**
+ *
+ * @author Goretti
+ */
+public class IS2_Practica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
