761370eb39686c241358108257b6a5c40312aecc
Goretti <Goretti@Goretti>
2015-10-15 10:28:58 +0100

    Practica 3 - Version con fichero.
    Se crea un histograma con datos procedentes de un fichero y se muestra con HistogramDisplay

--- src/is2_practica3/IS2_Practica3.java
+++ src/is2_practica3/IS2_Practica3.java
@@ -1,6 +1,10 @@
 
 package is2_practica3;
 
+import com.sun.corba.se.impl.encoding.BufferManagerRead;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author Goretti
@@ -9,28 +13,22 @@ public class IS2_Practica3 {
 
    public static void main(String[] args) {
        Histogram<String> histo = new Histogram<>();
+       String name = "C:\\Users\\Goretti\\Documents\\NetBeansProjects\\IS2_Practica3\\DATA\\emailsfilev1.txt";
        
-       histo.increment("gmail.com");
-       histo.increment("gmail.com");
-       histo.increment("gmail.com");
-       histo.increment("gmail.com");
-       histo.increment("gmail.com");
-       histo.increment("gmail.com");
-       
-       histo.increment("ulpgc.com");
-       histo.increment("ulpgc.com");
-       histo.increment("ulpgc.com");
-       histo.increment("ulpgc.com");
-       histo.increment("ulpgc.com");
-       histo.increment("ulpgc.com");
-       histo.increment("ulpgc.com");
-       
-       histo.increment("hotmail.com");
-       histo.increment("hotmail.com");
-       histo.increment("hotmail.com");
-       histo.increment("hotmail.com");
-       histo.increment("hotmail.com");
-       
+       try{
+           BufferedReader fileIn = new BufferedReader(new FileReader(name));
+           String mail;
+           
+           while ((mail = fileIn.readLine()) != null){
+               if (mail.contains("@")) {
+                   histo.increment(mail.split("@")[1]);
+                   System.out.println("sadkjf");
+               }
+           }
+           
+       } catch (Exception e){
+           
+       }
        
        HistogramDisplay histo1 = new HistogramDisplay(histo);
        histo1.execute();
